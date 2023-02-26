c04d7a88c1c25c6bfd67690a2fc743597ce29449
Psalishol samuel <psalishol80@gmail.com>
2023-02-26 23:23:15 +0100

    app asswt

---
+++ src/assets/index.ts
@@ -0,0 +1,18 @@
+export const scrollImage = 'https://www.linkpicture.com/q/scroll.png';
+
+export const bird = require('./bird.json');
+
+export const arrow = 'https://www.linkpicture.com/q/arrow_5.png';
+
+// Grass
+export const grass = 'https://www.linkpicture.com/q/grass.png';
+export const grassLight = 'https://www.linkpicture.com/q/grassLight.png';
+
+// Cloud
+export const cloud = 'https://www.linkpicture.com/q/cloud_11.png';
+export const cloudLight = 'https://www.linkpicture.com/q/cloud-light-mode.png';
+
+// LampPost
+export const lampPost =
+  'https://www.linkpicture.com/q/mPngtreemblack-acorn-shape-street-light_5729617.png';
+export const lampPostLight = 'https://www.linkpicture.com/q/lampPostLight.png';
