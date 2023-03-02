8e77c711272c822d311e2ecef6eadef94e8ded07
Psalishol samuel <psalishol80@gmail.com>
2023-03-02 19:29:05 +0100

    add lamp switch with mode change

--- web/src/components/molecules/LampPost.tsx
+++ web/src/components/molecules/LampPost.tsx
@@ -1,17 +1,19 @@
-import {lampPost} from '../../assets';
+import {lampPost, lampPostLight} from '../../assets';
+import {useAppMode} from '../../hooks';
 import {Image} from '../atom';
 import React from 'react';
 
 const LampPost: React.FunctionComponent = () => {
+  const {isLight} = useAppMode();
   return (
     <Image
       height={720}
       width={150}
       position="absolute"
       right={0}
-      bottom={0}
+      bottom={-40}
       resizeMode="cover"
-      source={{uri: lampPost}}
+      source={{uri: isLight ? lampPostLight : lampPost}}
     />
   );
 };
