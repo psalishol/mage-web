e1c0a11833b1ea4a03e3bb36dfc7d854da46dcb0
Psalishol samuel <psalishol80@gmail.com>
2023-03-03 04:48:27 +0100

    update prop with native pressable props: fixes onPress issue

--- web/src/components/atom/Pressable.ts
+++ web/src/components/atom/Pressable.ts
@@ -1,8 +1,11 @@
 import {createBox} from '@shopify/restyle';
 import {Theme} from '../../style/Theme';
-import {Pressable as RNPressable} from 'react-native';
+import {
+  Pressable as RNPressable,
+  PressableProps as RNPressableProps,
+} from 'react-native';
 
-const Pressable = createBox<Theme>(RNPressable);
+const Pressable = createBox<Theme, RNPressableProps>(RNPressable);
 
 export type PressableProps = React.ComponentProps<typeof Pressable>;
 
