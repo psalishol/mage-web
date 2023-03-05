9004aa068187592c2f3bbf73e69133570fc67e02
Psalishol samuel <psalishol80@gmail.com>
2023-03-05 14:07:00 +0100

    refactored code

--- web/src/components/organism/Header.tsx
+++ web/src/components/organism/Header.tsx
@@ -1,14 +1,27 @@
-import {View, Text} from 'react-native';
-import {HeaderMenuHandler} from '../molecules';
 import React from 'react';
-import styles from './Header.style';
+import {Box} from '../atom';
+import {RouterElement} from '../../router';
+import {useRTWindowDimension} from '../../hooks';
 
-const Header = () => {
+const Header: React.FunctionComponent = () => {
+  const {PHONE_SIZE} = useRTWindowDimension();
   return (
-    <View style={styles.container}>
-      <Text style={styles.logo}></Text>
-      <HeaderMenuHandler />
-    </View>
+    <Box
+      top={PHONE_SIZE ? 0 : '1%'}
+      right={0}
+      left={!PHONE_SIZE ? 0 : undefined}
+      bottom={!PHONE_SIZE ? undefined : 0}
+      justifyContent={'center'}
+      position={'absolute'}>
+      <Box
+        flexDirection={PHONE_SIZE ? undefined : 'row'}
+        alignSelf={'center'}
+        alignItems={'center'}>
+        <RouterElement link="/" name="Home" />
+        <RouterElement link="/about" name="About" />
+        <RouterElement link="/articles" name="Articles" />
+      </Box>
+    </Box>
   );
 };
 
